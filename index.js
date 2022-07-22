565d3b56b57e98910ed0385961aa9f20094e2763
Josh Man <joshuamangerino@gmail.com>
2022-07-22 11:25:52 -0600

    added prompts

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+// importing what I need to run the program
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
 const { createConnection } = require('net');
 
+// requiring the env file for confidential information
 require('dotenv').config();
 
+// creating the connection to mysql database
 const connectino = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -14,5 +17,24 @@ const connectino = mysql.createConnection({
 
 createConnection.connect(err => {
     if (err) throw err;
-    afterConnection();
+    promptUser();
 });
+
+const promptUser = () => {
+    inquirer.prompt ([
+        {
+            name: 'options',
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: [
+                'view all departments',
+                'view all roles',
+                'view all employess',
+                'add a department',
+                'add a role',
+                'add an employee',
+                'and update and employee role'
+            ]
+        }
+    ])
+}
